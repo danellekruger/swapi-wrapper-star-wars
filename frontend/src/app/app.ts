35100de1495f2071ce70ac75fe791d809f005d8b
Kru9erD <kruger.danelle2829@gmail.com>
2025-09-25 23:04:09 +0200

    Fixed blank page error

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FilmsListComponent } from './components/films-list/films-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
-  template: `<router-outlet></router-outlet>`
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    FilmsListComponent
+  ],
+  template: `
+    <h1>SWAPI Wrapper</h1>
+    <app-films-list></app-films-list>
+    <router-outlet></router-outlet>
+  `,
 })
 export class App {}
